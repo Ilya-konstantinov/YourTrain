5c4d0cfd759d16177e35405dcc27b86ccb9c0590
Konstantinov Ilya laptop <iliyakon2006@gmail.com>
2023-09-11 21:58:59 +0300

    first_add

--- TODO.md
+++
@@ -1,2 +0,0 @@
-// TODO: some do in my code //
-asa
--- path.py
+++ path.py
@@ -1,5 +1,5 @@
-from datetime import time, timedelta, datetime
-
+from datetime import timedelta, datetime
+from raw_req import get_whole_path
 class Station:
     """
     Класс станции
@@ -37,7 +37,7 @@ class Path:
         # То для корректного вычисления времени
         # Добавляем день к дате прибытия 
         if (departure_time > arrival_time) :
-            arrival_time += timedelta(minutes=0, days=0, microseconds=0, hours=1)
+            arrival_time += timedelta(minutes=0, days=1, microseconds=0, hours=0)
         
         self.arrival_time: timedelta = arrival_time
         self.path_time: timedelta = (arrival_time - departure_time)
@@ -52,22 +52,42 @@ class Path:
     
     def get_view(self) -> str:
         """Маршрут для отображения"""
-        
+        # Перобразование "сегодня" в timedelta
         now = datetime.now()
         now = timedelta(hours=now.hour, minutes=now.minute)
-        
+        # Форматирование
         start_tile = beauty_station(self.start_st)
         finish_title = beauty_station(self.finish_st)
-        dep_title = beauty_station(self.dep_st)
-        arr_tile = beauty_station(self.arr_st)
-        path_time = beauty_time(self.path_time)
-        first_line = f'{start_tile:<10}{" "*10}{finish_title:>10}'
-        second_line = f'{dep_title:<10}{path_time:^10}{arr_tile:>10}'
-        third_line = f'Через {beauty_time(self.departure_time - now )}'
-        return '\n'.join([first_line, second_line, third_line])
+        dep_title = beauty_station(self.dep_st, 12)
+        arr_tile = beauty_station(self.arr_st, 12)
+        # строчка времени пути        
+        st_time = f'{beauty_time(self.departure_time):_<10}'
+        path_time = f'{beauty_path_time(self.path_time):_^10}'
+        fn_time = f'{beauty_time(self.arrival_time):_>10}'
+        time = st_time + path_time + fn_time
+        
+        zero_line = f'{start_tile:_<10}{"_"*10}{finish_title:_>10}'
+        second_line = f'{dep_title:_<12}{"-"*6}{arr_tile:_>12}'
+        third_line = f'Через {beauty_path_time(self.departure_time - now )}'
+        return '\n'.join([zero_line, time, second_line, third_line])
     
+    def whole_path(self) -> list[str]:
+
+        stops: list[tuple[str|bool]] = get_whole_path(self.path_id)
+        for ind, st in enumerate(stops):
+            stops[ind] = (
+                            f"{st[0]} {'-'*4} {beauty_station(st[1])}",
+                            st[2]
+                        )
+
+        return stops
     
-def beauty_time(path_time: timedelta) -> str:
+def beauty_time(time: timedelta) -> str:
+    h = f'{time.seconds//3600:0>2}'
+    m = f'{time.seconds%3600//60:0>2}'
+    return f'{h}:{m}'
+
+def beauty_path_time(path_time: timedelta) -> str:
     """Конкретное время для отображения"""
     
     if isinstance(path_time,datetime):
